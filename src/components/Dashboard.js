2a928eaaa066d3318004f4227dc8a13bfdb6460b
Marina Ivanova <marina.ivanova.ca@gmail.com>
2023-09-28 21:15:32 -0700

    Setup Loading and Panel Component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,58 @@
 import React, { Component } from "react";
+import Loading from "./Loading";
+import Panel from "./Panel";
 
 import classnames from "classnames";
 
+const data = [
+  {
+    id: 1,
+    label: "Total Photos",
+    value: 10
+  },
+  {
+    id: 2,
+    label: "Total Topics",
+    value: 4
+  },
+  {
+    id: 3,
+    label: "User with the most uploads",
+    value: "Allison Saeng"
+  },
+  {
+    id: 4,
+    label: "User with the least uploads",
+    value: "Lukas Souza"
+  }
+];
+
 class Dashboard extends Component {
+  state = {
+    loading: false
+  };
+
   render() {
     const dashboardClasses = classnames("dashboard");
 
-    return <main className={dashboardClasses} />;
+    if (this.state.loading) {
+      return <Loading />;
+    }
+
+    // Map over the data array and create a Panel component for each item
+    const panels = data.map((panel) => (
+      <Panel
+        key={panel.id} // Use panel.id as the key
+        id={panel.id} // Pass panel.id as a prop
+        label={panel.label} // Pass panel.label as a prop
+        value={panel.value} // Pass panel.value as a prop
+      />
+    ));
+
+    return (<main className={dashboardClasses}>
+      {panels}
+    </main>
+    );
   }
 }
 
